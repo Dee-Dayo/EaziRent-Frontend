6a48fbdcc6b8aa2abf2bb372bdfa173dbd7cda93
Dayo Akinyemi <darhyor2050@gmail.com>
2024-07-25 18:15:22 +0100

    started hero

--- src/pages/Home/Hero/index.jsx
+++ src/pages/Home/Hero/index.jsx
@@ -1,8 +1,14 @@
+import picture from "../../../assets/hero.png"
+import style from "./index.module.css";
+
 const Hero = () => {
     return (
-        <div>
-            <h1>GOOD MORNING</h1>
-            <p>How are you</p>
+        <div className={style.heroSection}>
+            <div>
+                <h1>Let's hunt for your dream residence</h1>
+                <p>Explore our range of beautiful properties with separate accommodation suitable for you.</p>
+            </div>
+            <img src={picture} alt="image"/>
         </div>
     )
 }
