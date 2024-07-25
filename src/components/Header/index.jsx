f131421e1709707cbbe8c8c4d18301c0633072db
Dayo Akinyemi <darhyor2050@gmail.com>
2024-07-25 11:01:58 +0100

    started project

---
+++ src/components/Header/index.jsx
@@ -0,0 +1,9 @@
+const Header = () => {
+    return (
+        <>
+        <h1>Good morning</h1>
+        </>
+    )
+}
+
+export default Header;
---
+++ src/pages/Home/Hero/index.jsx
@@ -0,0 +1,10 @@
+const Hero = () => {
+    return (
+        <div>
+            <h1>GOOD MORNING</h1>
+            <p>How are you</p>
+        </div>
+    )
+}
+
+export default Hero;
