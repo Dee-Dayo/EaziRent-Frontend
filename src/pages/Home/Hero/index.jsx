3d83fb94f86c0fdbe5643a90d6ed561e5c039329
Dayo Akinyemi <darhyor2050@gmail.com>
2024-08-02 11:26:28 +0100

    complete hero session

--- src/pages/Home/Hero/index.jsx
+++ src/pages/Home/Hero/index.jsx
@@ -1,14 +1,33 @@
-import picture from "../../../assets/hero.png"
+import React from 'react';
+import picture from "../../../assets/hero.png";
 import style from "./index.module.css";
+import FilledButton from "../../../components/FilledButton";
 
 const Hero = () => {
     return (
         <div className={style.heroSection}>
-            <div>
-                <h1>Let's hunt for your dream residence</h1>
-                <p>Explore our range of beautiful properties with separate accommodation suitable for you.</p>
+            <div className={style.header}>
+                <div>
+                    <h1>Let's hunt for your dream residence</h1>
+                    <p>Explore our range of beautiful properties with separate accommodation suitable for you.</p>
+                </div>
+                <div className={style.searchArea}>
+                    <input type="text" placeholder="Location"/>
+                    <select name="type">
+                        <option value="">Condo</option>
+                        <option value="">Duplex</option>
+                        <option value="">Hostel</option>
+                        <option value="">Bungalow</option>
+                    </select>
+                    <select name="price-range">
+                        <option value="">100k - 450k</option>
+                        <option value="">500k - 1Mk</option>
+                        <option value="">1.5Mk - 3Mk</option>
+                    </select>
+                    <FilledButton name={"Search"}/>
+                </div>
             </div>
-            <img src={picture} alt="image"/>
+            <img src={picture} alt="house"/>
         </div>
     )
 }
