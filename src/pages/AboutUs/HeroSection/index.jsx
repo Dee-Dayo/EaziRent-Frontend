cc7aa20c4a304711f848a306b308a4f48d1ba719
Victor-Maverick <victormsonter@gmail.com>
2024-08-09 06:24:05 +0100

    commit

--- src/pages/AboutUs/HeroSection/index.jsx
+++ src/pages/AboutUs/HeroSection/index.jsx
@@ -7,11 +7,12 @@ const HeroSection = () => {
                 <img src={image1} alt={"image"}/>
             </div>
 
-            <div>
-                <h2>Your peace of mind,<br/>our priority</h2>
-                <p>Curabitur efficitur ante vel mi bibendum, et maximus nisl ultricies. Morbi nec tempus dui, sit amet facilisis nisl.
-                    Curabitur efficitur luctus enim nec sollicitudin. Fusce venenatis venenatis lorem eu accumsan. Nunc sit amet mi vitae odio porttitor feugiat. Sed quis sem elementum, viverra ligula at, lobortis magna. Praesent congue nibh in dolor rutrum, et euismod quam elementum.
-                    Mauris justo felis, iaculis quis sagittis sit amet, feugiat vitae est. Aliquam porta ex elit, sit amet rutrum turpis egestas viverra. Quisque at libero purus.</p>
+            <div className={style.text_section}>
+                <h1>Your peace of mind,<br/>our priority</h1>
+                <p>Take the hassle out of property management with our all-in-one rental app.
+                </p>
+                <p>From listing properties to managing tenant interactions and processing payments, everything you need is just a tap away.</p>
+                <p>Designed for both landlords and tenants, our app simplifies the rental process, saving you time and effort. Experience a smarter, more efficient way to manage your rentals—get started today!</p>
             </div>
         </div>
     )
