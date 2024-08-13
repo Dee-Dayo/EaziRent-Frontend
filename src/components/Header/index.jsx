ee6bd8f4dbd25654a7c158953fe6135536ed738f
Dayo Akinyemi <darhyor2050@gmail.com>
2024-08-13 12:33:32 +0100

    started mobile responsiveness

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,81 +1,102 @@
 import style from "./index.module.css";
-import logo from "../../assets/logo.png"
+import logo from "../../assets/logo.png";
 import FilledButton from "../FilledButton";
-import React, {useState} from "react";
-import Hamburger from "hamburger-react"
-import {useNavigate} from "react-router-dom";
-
-
+import React, { useState, useEffect } from "react";
+import Hamburger from "hamburger-react";
+import { useNavigate } from "react-router-dom";
 
 const Header = () => {
-    const isMobile = window.innerWidth <= 1080;
+    const [isMobile, setIsMobile] = useState(window.innerWidth <= 1080);
     const [isOpen, setIsOpen] = useState(false);
 
     const navigate = useNavigate();
+
+    useEffect(() => {
+        const handleResize = () => {
+            setIsMobile(window.innerWidth <= 1080);
+        };
+
+        window.addEventListener("resize", handleResize);
+
+        return () => {
+            window.removeEventListener("resize", handleResize);
+        };
+    }, []);
+
     const handleLogin = () => {
         navigate("/login");
     };
 
     const handleLogoClick = () => {
         navigate("/home");
-    }
-    const handleAboutClick = ()=>{
+    };
+
+    const handleAboutClick = () => {
         navigate("/about");
-    }
-    const handleContactClick =()=>{
+    };
+
+    const handleContactClick = () => {
         navigate("/contact");
-    }
+    };
+
     const handlePropertiesClick = () => {
-        navigate("/properties")
-    }
+        navigate("/properties");
+    };
 
     return (
-        <>
-            <div className={style.nav}>
-                <div className={style.image} onClick={handleLogoClick}>
-                    <img src={logo} alt="logo"/>
-                </div>
-
-                {isMobile ? (
-                    <>
-                        <div className={style.hamburger}>
-                            <Hamburger toggled={isOpen} toggle={setIsOpen} color="#5cb30b"/>
-                        </div>
-                        {isOpen && (
-                            <div className={style.mobileMenu}>
-                                <div className={style.midSection}>
-                                    <p><a href="/home" style={{textDecoration: "none", color: "#4d4d4d"}}> Home</a></p>
-                                    <p onClick={handleAboutClick}>About Us</p>
-                                    <p><a href="/service" style={{textDecoration: "none", color: "#4d4d4d"}}>Our
-                                        Service</a></p>
-                                </div>
-
-                                <div className={style.btn}>
-                                    <p className={style.loginBtn}><a href="/login">Login</a></p>
-                                    <FilledButton name={"Sign Up"}/>
-                                </div>
-                            </div>
-                        )}
-                    </>
-                ) : (
-                    <>
-                        <div className={style.midSection}>
-                            <p onClick={handleLogoClick}> Home</p>
-                            <p onClick={handlePropertiesClick}>Properties</p>
-                            <p onClick={handleAboutClick}>About Us</p>
-                            <p>Our Service</p>
-                            <p onClick={handleContactClick}>Contact</p>
-                        </div>
-
-                        <div className={style.btn}>
-                            <p className={style.loginBtn} onClick={handleLogin}>Login</p>
-                            <FilledButton name={"Sign Up"} whereTo={"signup"}/>
-                        </div>
-                    </>
-                )}
+        <div className={style.nav}>
+            <div className={style.image} onClick={handleLogoClick}>
+                <img src={logo} alt="logo" />
             </div>
-        </>
-    )
-}
+
+            {isMobile ? (
+                <>
+                    <div className={style.hamburger}>
+                        <Hamburger toggled={isOpen} toggle={setIsOpen} color="#5cb30b" />
+                    </div>
+                    {isOpen && (
+                        <div className={style.mobileMenu}>
+                            <div className={style.midSection}>
+                                <p>
+                                    <a href="/home" style={{ textDecoration: "none", color: "#4d4d4d" }}>
+                                        Home
+                                    </a>
+                                </p>
+                                <p onClick={handleAboutClick}>About Us</p>
+                                <p>
+                                    <a href="/service" style={{ textDecoration: "none", color: "#4d4d4d" }}>
+                                        Our Service
+                                    </a>
+                                </p>
+                            </div>
+
+                            <div className={style.btn}>
+                                <p className={style.loginBtn}>
+                                    <a href="/login">Login</a>
+                                </p>
+                                <FilledButton name={"Sign Up"} />
+                            </div>
+                        </div>
+                    )}
+                </>
+            ) : (
+                <>
+                    <div className={style.midSection}>
+                        <p onClick={handleLogoClick}> Home</p>
+                        <p onClick={handlePropertiesClick}>Properties</p>
+                        <p onClick={handleAboutClick}>About Us</p>
+                        <p>Our Service</p>
+                        <p onClick={handleContactClick}>Contact</p>
+                    </div>
+
+                    <div className={style.btn}>
+                        <p className={style.loginBtn} onClick={handleLogin}>Login</p>
+                        <FilledButton name={"Sign Up"} whereTo={"signup"} />
+                    </div>
+                </>
+            )}
+        </div>
+    );
+};
 
 export default Header;
