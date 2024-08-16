323ef9734756aa9bcd1deb6085ad4e377c73065e
Victor-Maverick <victormsonter@gmail.com>
2024-08-16 12:51:28 +0100

    commit

--- src/pages/Properties/LandlordProperties.js
+++ src/pages/Properties/LandlordProperties.js
@@ -15,7 +15,7 @@ const LandlordProperties = () => {
     useEffect(() => {
         const fetchProperties = async () => {
             try {
-                const response = await axios.get(`https://eazirent-latest.onrender.com/api/v1/property/findByLanlordId/${landlordId}`);
+                const response = await axios.get(`https://eazirent-latest.onrender.com/api/v1/property/findByLanlordId`);
                 setProperties(response.data.properties);
             } catch (error) {
                 console.error('Error fetching properties:', error);
