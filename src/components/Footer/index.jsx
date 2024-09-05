50b43c095d24011d00b297aacb717f6b0debe539
Dayo Akinyemi <darhyor2050@gmail.com>
2024-09-05 18:26:10 +0100

    fixed header, added spinner to payment page

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -1,30 +1,23 @@
-import logo from "../../assets/logo.png"
-import facebook from "../../assets/facebook-f 1.png"
-import insta from "../../assets/instagram 1.png"
-import twitter from "../../assets/twitter 1.png"
-import style from "./index.module.css"
+import logo from "../../assets/logo.png";
+import facebook from "../../assets/facebook-f 1.png";
+import insta from "../../assets/instagram 1.png";
+import twitter from "../../assets/twitter 1.png";
+import style from "./index.module.css";
 
 const Footer = () => {
     const year = new Date().getFullYear();
 
     return (
-        <>
-            <div className={style.footer}>
-                <div>
-                    <img src={logo} alt="logo"/>
-                </div>
+        <div className={style.footer}>
+            <img src={logo} alt="logo" className={style.logo}/>
+            <p className={style.copyright}>Copyright © {year} EaziRent. All rights reserved</p>
+            <div className={style.icons}>
+                <img src={insta} alt="Instagram" />
+                <img src={facebook} alt="Facebook" />
+                <img src={twitter} alt="Twitter" />
             </div>
-            <div className={style.footerCopyright}>
-                <p>Copyright © {year} EaziRent. All rights reserved</p>
-                <div className={style.icons}>
-                    <img src={insta} alt="icon"/>
-                    <img src={facebook} alt="icon"/>
-                    <img src={twitter} alt="icon"/>
-                </div>
-            </div>
-
-        </>
-    )
-}
+        </div>
+    );
+};
 
 export default Footer;
